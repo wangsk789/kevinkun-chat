751cbeb7dee1d659b417bb898433156ae3710692
wangsk789 <wangsk789@163.com>
2019-12-24 20:56:25 +0800

    add roomFull event

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.get('/', function(req, res){
 
 var roomInfo = {};
 
+
 io.on('connection', function (socket) {
 
   var roomID = '';   
   var user = '';
-
+	var isInRoom = false;
   socket.on('join', function (roomName, userName) {
     user = userName;
 	roomID = roomName;
@@ -23,18 +24,25 @@ io.on('connection', function (socket) {
     if (!roomInfo[roomID]) {
       roomInfo[roomID] = [];
     }
-    roomInfo[roomID].push(user);
-    socket.join(roomID);    // 加入房间
-	
-    // 通知用户已经加入
-	socket.emit("joinedRoom",roomInfo[roomID]);
-    // 通知房间内其它人员
-    socket.broadcast.to(roomID).emit('otherJoinedRoom', user, roomInfo[roomID]);  
-    console.log(user + '加入了' + roomID);
+	if(roomInfo[roomID].length > 1){
+		socket.emit("roomFull",roomID);
+	}else{
+		roomInfo[roomID].push(user);
+		socket.join(roomID); 
+		isInRoom = true;
+		
+		socket.emit("joinedRoom",roomInfo[roomID]);
+		socket.broadcast.to(roomID).emit('otherJoinedRoom', user, roomInfo[roomID]);  
+		console.log(user + '加入了' + roomID);
+	}
+    
   });
 
   socket.on('leave', function () {
      // 从房间名单中移除
+	if(!isInRoom){
+		 return false;
+	}
     var index = -1;
 	if (roomInfo[roomID]){
 		var index = roomInfo[roomID].indexOf(user);
@@ -43,7 +51,7 @@ io.on('connection', function (socket) {
     if (index !== -1) {
       roomInfo[roomID].splice(index, 1);
     }
-
+	isInRoom =false;
     socket.leave(roomID);    // 退出房间
     socket.broadcast.to(roomID).emit('otherLeftRoom', user, roomInfo[roomID]); 
     console.log(user + '退出了' + roomID);
@@ -51,6 +59,9 @@ io.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
     // 从房间名单中移除
+	if(!isInRoom){
+		 return false;
+	}
     var index = -1;
 	if (roomInfo[roomID]){
 		var index = roomInfo[roomID].indexOf(user);
@@ -59,7 +70,7 @@ io.on('connection', function (socket) {
     if (index !== -1) {
       roomInfo[roomID].splice(index, 1);
     }
-
+	isInRoom =false;
     socket.leave(roomID);    // 退出房间
     socket.broadcast.to(roomID).emit('otherLeftRoom', user, roomInfo[roomID]); 
     console.log(user + '退出了' + roomID);
@@ -67,6 +78,9 @@ io.on('connection', function (socket) {
 
 
   socket.on('message', function (eventName, eventMessage) {
+	if(!isInRoom){
+		 return false;
+	}
     // 验证如果用户不在房间内则不给发送
     if (roomInfo[roomID].indexOf(user) === -1) {  
       return false;
